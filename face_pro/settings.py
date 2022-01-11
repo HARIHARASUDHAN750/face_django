581b00440e1c0130987d6cb0195d7954053a27af
Hariharasudhan <58838759+HARIHARASUDHAN750@users.noreply.github.com>
2022-01-10 18:37:59 -0800

    Update settings.py

--- face_pro/settings.py
+++ face_pro/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS =[stat_dir,]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 MEDIA_URL = '/media/'
